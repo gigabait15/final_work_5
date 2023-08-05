9f3b42078faa571253e36f20cadbf27f5bd49899
gigabait <Workkerbecov@yandex.ru>
2023-08-05 15:21:17 +0300

    Готовый проект для сдачи

--- DBManager.py
+++ DBManager.py
@@ -1,28 +1,107 @@
-
+import psycopg2
+from POSTGRES import POSTGRES
 
 
 class DBManager:
 
-    def get_companies_and_vacancies_count(self):
+    @staticmethod
+    def get_companies_and_vacancies_count():
         """Получает список всех компаний и
         количество вакансий у каждой компании."""
-        pass
+        params = POSTGRES.config()
+        with psycopg2.connect(**params, database="hh_emp") as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                query = """
+                            SELECT name_emp, open_vacancies FROM employees
+                            """
+                cursor.execute(query)
+                for item in cursor.fetchall():
+                    print(f"название компании: {item[0]} \nколичество вакансий у компании - {item[1]}\n")
 
-    def get_all_vacancies(self):
+    @staticmethod
+    def get_all_vacancies():
         """Получает список всех вакансий с указанием названия компании,
          названия вакансии и зарплаты и ссылки на вакансию."""
-        pass
+        params = POSTGRES.config()
+        with psycopg2.connect(**params, database="hh_emp") as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                query = """
+                            SELECT employees.name_emp, vacancies.name_vac,
+                            vacancies.salary, vacancies.url FROM vacancies 
+                            INNER JOIN employees ON employees.employees_id = vacancies.employees_id
+                            """
+                cursor.execute(query)
+                for item in cursor.fetchall():
+                    print(f"Название компании : {item[0]}\nНазвание вакансии : {item[1]}\n"
+                          f"Заработная плата - {item[2]} руб.\nссылка на вакансию :{item[3]}\n")
 
-    def get_avg_salary(self):
+    @staticmethod
+    def get_avg_salary():
         """Получает среднюю зарплату по вакансиям."""
-        pass
+        params = POSTGRES.config()
+        with psycopg2.connect(**params, database="hh_emp") as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                query = """
+                            SELECT AVG(salary) as average_salary FROM vacancies
+                            """
+                cursor.execute(query)
+                result = cursor.fetchone()
+                average_salary = int(result[0])
+                print(f"Средняя зарплата: {average_salary} руб.")
 
-    def get_vacancies_with_higher_salary(self):
+    @staticmethod
+    def get_vacancies_with_higher_salary():
         """Получает список всех вакансий,
         у которых зарплата выше средней по всем вакансиям."""
-        pass
+        params = POSTGRES.config()
+        with psycopg2.connect(**params, database="hh_emp") as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                subquery = """
+                                SELECT AVG(salary) as average_salary
+                                FROM vacancies
+                                """
+                cursor.execute(subquery)
+                result = cursor.fetchone()
+                average_salary = int(result[0])
 
-    def get_vacancies_with_keyword(self):
+                query = """
+                            SELECT vac_id, name_vac, url, salary
+                            FROM vacancies
+                            WHERE salary > %s
+                            """
+                cursor.execute(query, (average_salary,))
+                results = cursor.fetchall()
+                print(f"Список вакансий у которых зарплата выше {average_salary} руб.\n")
+                for item in results:
+                    print(f"Название вакансии: {item[1]}\nЗарплата- {item[3]}руб.\nссылка на вакансию: {item[2]}\n")
+
+    @staticmethod
+    def get_vacancies_with_keyword():
         """Получает список всех вакансий,
         в названии которых содержатся переданные в метод слова, например “python”. """
-        pass
+        user_input = input("Введите название вакансии:")
+        print()
+        params = POSTGRES.config()
+        with psycopg2.connect(**params, database="hh_emp") as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                params = POSTGRES.config()
+                with psycopg2.connect(**params, database="hh_emp") as conn:
+                    conn.autocommit = True
+                    with conn.cursor() as cursor:
+                        query = """
+                                    SELECT vac_id, name_vac, url, salary
+                                    FROM vacancies
+                                    WHERE name_vac LIKE %s
+                                    """
+                        search_keyword = f"%{user_input}%"
+                        cursor.execute(query, (search_keyword,))
+                        results = cursor.fetchall()
+                        print(f"Все вакансии по запросу - {user_input}\n")
+                        for item in results:
+                            print(f"Название вакансии: {item[1]}\nСсылка на вакансию: "
+                                  f"{item[2]}\nЗарплата- {item[3]}руб.\n")
--- queries.sql
+++ queries.sql
@@ -1,28 +1,9 @@
---- установка параметров для текущей сессии
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---- удаляем таблицы, если они существуют
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS vacancies;
-
---- создаем таблицы со столбцами
---- таблица employees
 CREATE TABLE employees (
     employees_id INT PRIMARY KEY,
     name_emp VARCHAR(100) NOT NULL,
     url VARCHAR(100) NOT NULL,
     open_vacancies INT NOT NULL
 );
---- таблица  vacancies
 CREATE TABLE vacancies (
     vac_id INT PRIMARY KEY,
     name_vac VARCHAR(100) NOT NULL,
@@ -30,4 +11,4 @@ CREATE TABLE vacancies (
     salary INT NOT NULL,
     employees_id INT,
     FOREIGN KEY (employees_id) REFERENCES employees(employees_id)
-)
+);
